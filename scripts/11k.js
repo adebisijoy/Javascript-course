b47d7cd483a4f807992cde4eaea1509ebeef4cd7
Adebisi Joy <greenjoy234@gmail.com>
2023-07-05 14:37:40 +0100

    Practiced arrays and loop exercises

---
+++ scripts/11k.js
@@ -0,0 +1,12 @@
+function countPositive(nums) {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+     if (nums[i] > 0) {
+       result++;
+     }
+  }
+  return result;
+}
+console.log(countPositive([1, -3, 5]));
+console.log(countPositive([-2, 3, -5, 7, 10]));
