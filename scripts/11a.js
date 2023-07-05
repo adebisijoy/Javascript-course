8dd230ceca9997d8ba2e275282292608752d7bb4
Adebisi Joy <greenjoy234@gmail.com>
2023-07-05 14:42:11 +0100

    Array exercise

---
+++ scripts/11a.js
@@ -0,0 +1,27 @@
+const nums = [10, 20, 30];
+    console.log(nums);
+
+    nums[2] = 99;
+    console.log(nums);
+
+    function getLastValue(array) {
+      const lastIndex = array.length - 1;
+      return array[lastIndex];
+    }
+    console.log(getLastValue([1, 20, 22, 24, 5]));
+    console.log(getLastValue(['hi', 'hello', 'good']));
+
+    function arraySwap(array) {
+      const lastIndex = array.length - 1;
+
+      const lastValue = array[lastIndex];
+      const firstValue = array[0];
+
+      array[0] = lastValue;
+      array[lastIndex] = firstValue;
+
+      return array;
+    }
+
+    console.log(arraySwap([1, 20, 22, 24, 5]));
+    console.log(arraySwap(['hi', 'hello', 'good']));
